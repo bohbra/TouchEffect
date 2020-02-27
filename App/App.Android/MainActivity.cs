952a25df448f9f5ce6805db7e0580b888cf17789
Robert Gaede <robert.gaede@bd.com>
2020-02-27 13:26:52 -0800

    Add preserve

--- App/App.Android/MainActivity.cs
+++ App/App.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using TouchEffect.Android;
 
 namespace App.Droid
 {
@@ -21,6 +22,7 @@ namespace App.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            TouchEffectPreserver.Preserve();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
